d25cbad78a9ce307a45aa465c657004f67626a24
oskar951 <ceremnovas@hotmail.com>
2020-06-09 12:09:36 +0000

    jenkins changes

--- script/docker.sh
+++ script/docker.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+docker stack deploy --compose-file docker-compose.yml stackdemo
--- script/installation.sh
+++ script/installation.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-source /var/lib/jenkins/.bashrc
+#install python3 and pip
+
+apt install -y python 
+
+apt install -y python-pip
+
+# make sure ~/.local/bin exists and is on your PATH
+mkdir -p ~/.local/bin
+touch ~/.bashrc
+echo 'PATH=$PATH:~/.local/bin' > ~/.bashrc
+## change ownership
+sudo chown -R $(whoami):$(whoami) ~/*
+source ~/.bashrc
+## install ansible with pip
+pip install --user ansible
+# check that ansible has been installed
+ansible --version
