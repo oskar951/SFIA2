0ea2c8c808a45590477f2a5a709d2d16d60fe98a
oskar951 <ceremnovas@hotmail.com>
2020-06-10 15:06:48 +0000

    jenkins changes

--- script/installation.sh
+++ script/installation.sh
@@ -6,10 +6,12 @@ apt install -y python
 
 apt install -y python-pip
 
+cat ~/.bashrc > install.log
+
 # make sure ~/.local/bin exists and is on your PATH
 mkdir -p ~/.local/bin
 touch ~/.bashrc
-echo 'PATH=$PATH:~/.local/bin' > ~/.bashrc
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 ## change ownership
 sudo chown -R $(whoami):$(whoami) ~/*
 source ~/.bashrc
@@ -18,3 +20,5 @@ source ~/.bashrc
 pip3 install --user ansible
 # check that ansible has been installed
 ansible --version
+
+cat ~/.bashrc > install.log2
