81b817cd4e8b64ff153125c591897d5b85e45d4d
oskar951 <ceremnovas@hotmail.com>
2020-06-12 09:46:36 +0000

    changes to files

--- script/docker.sh
+++ script/docker.sh
@@ -15,7 +15,11 @@ docker push oskar951/service3
 docker push oskar951/service4
 docker push oskar951/nginxservice
 
-
+docker service update --force --update-parallelism 1 --update-delay 30s --image oskar951/service1
+docker service update --force --update-parallelism 1 --update-delay 30s --image oskar951/service2
+docker service update --force --update-parallelism 1 --update-delay 30s --image oskar951/service3
+docker service update --force --update-parallelism 1 --update-delay 30s --image oskar951/service4
+docker service update --force --update-parallelism 1 --update-delay 30s --image oskar951/nginxservice
 
 docker stack deploy --compose-file docker-compose.yaml stackdemo
 
